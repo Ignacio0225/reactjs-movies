946ec550f11e744aed2c0685b8a76353cc6a100b
유선혁 <tjsgur2399@icloud.com>
2025-02-17 23:13:57 +0900

    0217-3

--- src/App.js
+++ src/App.js
@@ -10,11 +10,12 @@ import {
 import Home from "./routes/Home";
 import Detail from "./routes/Detail";
 
+
 function App() {
     return <Router>
         <Routes>
             <Route path="/movie/:id" element={<Detail/>}></Route>
-            <Route path="/" element={<Home/>}></Route>
+            <Route path={`${process.env.PUBLIC_URL}/`}  element={<Home/>}></Route>
         </Routes>
     </Router>;
 }
